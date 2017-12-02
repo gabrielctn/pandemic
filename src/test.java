ec70045e77c95c8de503c423d8153b8d11033928
gabrielctn <gabrielcretin@gmail.com>
2017-12-02 21:27:01 +0100

    test adjacency matrix

--- src/test.java
+++ src/test.java
@@ -1,38 +1,20 @@
-import java.io.File;
-import java.io.FileReader;
+
 import java.io.IOException;
-import java.util.Scanner;
 
 public class test {
 
 	public static void main(String[] args) {
-		String content = null;
-	    String filename = "graph.txt";
-		File file = new File(filename );
-	    FileReader reader = null;
+		
+	    String fileName = "graph.txt";
+	    ConstructGraphe g = null;
 	    try {
-	        reader = new FileReader(file);
-	        char[] chars = new char[(int) file.length()];
-	        reader.read(chars);
-	        content = new String(chars);
-	        reader.close();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } finally {
-	        	try {
-	        		if(reader != null){
-	        			reader.close();
-	        		}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-	    }
-	    System.out.println(content);
+	    	g = new ConstructGraphe(fileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	   
+	   
 	    
-	    
-	    Scanner in = new Scanner(new FileReader(file));
-	    int integer = in.nextInt();
-	    System.out.println(integer);
 	}
 
 }
